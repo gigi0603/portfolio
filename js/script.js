7cf5199cc7a43bf1d81c225fff37446d3e89157a
gigi0603 <139085411+gigi0603@users.noreply.github.com>
2023-09-11 20:28:51 +0900

    JS変更

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 $(".openbtn").click(function () {//ボタンがクリックされたら
     $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
       $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-  });
-  
-  $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-      $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-      $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+      $("#js_overlay").toggleClass('on');//オーバーレイのonクラスを付与
+});
 
-      
-  });
+$("#js_overlay").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(this).removeClass('on');//#js_overlay自体のonを除去し
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+
+});
