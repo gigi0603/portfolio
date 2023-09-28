25bca96a74df7b70f1c2146ffed5aa95f1e2e38a
gigi0603 <139085411+gigi0603@users.noreply.github.com>
2023-09-28 22:00:31 +0900

    favicon編集

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 $(".openbtn").click(function () {//ボタンがクリックされたら
     $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-      $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-      $("#js_overlay").toggleClass('on');//オーバーレイのonクラスを付与
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $("#js_overlay").toggleClass('on');//オーバーレイのonクラスを付与
 });
 
-$("#js_overlay").click(function () {//ナビゲーションのリンクがクリックされたら
+$("#g-nav-list a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $("#js_overlay").removeClass('on');//#js_overlay自体のonを除去し
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+$("#js_overlay").click(function () {//オーバーレイがクリックされたら
     $(this).removeClass('on');//#js_overlay自体のonを除去し
     $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
-
 });
